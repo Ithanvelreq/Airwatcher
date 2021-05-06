3b2f63ad4fca190b5bc78f5022ace876872c297b
tmdr21 <t.mdarhri@gmail.com>
2021-05-06 17:47:58 +0200

    Classes purificateur, sensor, timestamp, mesure, attribut

---
+++ Mesure.h
@@ -0,0 +1,66 @@
+/*************************************************************************
+                           Mesure  -  description
+                             -------------------
+    début                : 05/05/2021
+    copyright            : (C) 2021 par VELARDE REQUENA / MDARHRI / FABREGUES / MARION
+    e-mail               : ithan.velarde-requena@insa-lyon.fr taha.mdarhri@insa-lyon.fr
+			   emilien.marion@insa-lyon.fr    thomas.fabregues@insa-lyon.fr
+*************************************************************************/
+
+//---------- Interface de la classe <Mesure> (fichier Mesure.h) ----------------
+#if ! defined ( MESURE_H )
+#define MESURE_H
+//--------------------------------------------------- Interfaces utilisées
+#include "Timestamp.h"
+#include <string>
+#include <iostream>
+using namespace std;
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+// Rôle de la classe <MESURE>
+//Permet de créer des objets de type Mesure qui donne des informations sur 
+//le type de mesure, sa valeur et la date de réalistion de cette mesure.
+//Elle est capable de créer ces objets en lisant un fichier csv
+//
+//------------------------------------------------------------------------
+
+class Mesure
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+
+// type Méthode ( liste des paramètres );
+
+Timestamp * getDate();
+string getSensorID();
+string getAttributeID();
+double getValue();
+
+//-------------------------------------------- Constructeurs - destructeur
+	Mesure(Timestamp * d, string capteur, string attribut, double valeur);
+	//Constructeur de la classe Mesure
+
+	~Mesure();
+	//Destructeur de la classe Mesure
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Mesures protégés
+
+	Timestamp * date;
+	string sensorID;
+	string attributeID;
+	double value;
+
+//-------------------------------- Autres définitions dépendantes de <Mesure>
+
+};
+
+#endif // MESURE_H
+
---
+++ Purificateur.h
@@ -0,0 +1,68 @@
+/*************************************************************************
+                           Purificateur  -  description
+                             -------------------
+    début                : 05/05/2021
+    copyright            : (C) 2021 par VELARDE REQUENA / MDARHRI / FABREGUES / MARION
+    e-mail               : ithan.velarde-requena@insa-lyon.fr taha.mdarhri@insa-lyon.fr
+			   emilien.marion@insa-lyon.fr    thomas.fabregues@insa-lyon.fr
+*************************************************************************/
+
+//---------- Interface de la classe <Purificateur> (fichier Purificateur.h) ----------------
+#if ! defined ( PURIFICATEUR_H )
+#define PURIFICATEUR_H
+//--------------------------------------------------- Interfaces utilisées
+#include "Timestamp.h"
+#include <string>
+using namespace std;
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+// Rôle de la classe <PURIFICATEUR>
+//Permet de créer des objets de type Purificateur qui donne des informations sur 
+//sa localisation et sa durée de fonctionnement
+//Elle est capable de créer ces objets en lisant un fichier csv
+//
+//------------------------------------------------------------------------
+
+class Purificateur
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+
+// type Méthode ( liste des paramètres );
+
+string getPurificateurID();
+double getLatitude();
+double getLongitude();
+Timestamp * getStart();
+Timestamp * getStop();
+
+
+//-------------------------------------------- Constructeurs - destructeur
+	Purificateur(string id, double la, double lo, Timestamp * start, Timestamp * stop);
+	//Constructeur de la classe Purificateur
+
+	~Purificateur();
+	//Destructeur de la classe Purificateur
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Purificateurs protégés
+
+	string purificateurID;
+	double latitude;
+	double longitude;
+	Timestamp * startTimestamp;
+	Timestamp * stopTimestamp;
+
+//-------------------------------- Autres définitions dépendantes de <Purificateur>
+};
+#endif // PURIFICATEUR_H
+
+
+
